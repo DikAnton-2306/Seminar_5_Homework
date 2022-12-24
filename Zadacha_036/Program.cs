63d83f3dc2c76bdffa1148d2e297fdc03b97ee45
Антон Дик <antondik@yandex.ru>
2022-12-24 20:38:06 +0300

    Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.

---
+++ Zadacha_036/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+System.Console.WriteLine("Укажите размер массива: ");
+int SIZE = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Укажите левую границу цифр: ");
+int LEFT_RANGE = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Укажите правую границу цифр: ");
+int RIGHT_RANGE = int.Parse(Console.ReadLine());
+
+int[] array = new int[SIZE];
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1); // [-9; 10)
+    }
+    return arr;
+}
+
+int SumNumbersOddIndex (int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i = i + 2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+array = FillArrayWithRandomNumbers(SIZE, LEFT_RANGE, RIGHT_RANGE);
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+int sum = SumNumbersOddIndex(array);
+System.Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях = {sum}");
